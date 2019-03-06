fdef6e4d6c54224fdaf25fcf78f6a9131040449e
liang <liang_1214@126.com>
2019-03-06 22:58:02 +0800

    fix

--- mysite/settings.py
+++ mysite/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = '4+u$j!1(%5aylp6ps=_1b3+*%sv^xv&s#%qnr+^ydm&eilhf*-'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
